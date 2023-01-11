7292af5b44fa9bbbdf36a92c56e3bf21049cb5f5
Greg Miller <gsmiller@gmail.com>
2023-01-11 08:45:27 -0800

    i think this is actually working

--- lucene/sandbox/src/java/org/apache/lucene/sandbox/queries/TermInSetQuery.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/queries/TermInSetQuery.java
@@ -3,6 +3,7 @@ package org.apache.lucene.sandbox.queries;
 import org.apache.lucene.index.DocValues;
 import org.apache.lucene.index.DocValuesType;
 import org.apache.lucene.index.FieldInfo;
+import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
 import org.apache.lucene.index.PostingsEnum;
 import org.apache.lucene.index.SortedSetDocValues;
@@ -14,24 +15,19 @@ import org.apache.lucene.search.ConstantScoreScorer;
 import org.apache.lucene.search.ConstantScoreWeight;
 import org.apache.lucene.search.DisiPriorityQueue;
 import org.apache.lucene.search.DisiWrapper;
-import org.apache.lucene.search.DocIdSet;
 import org.apache.lucene.search.DocIdSetIterator;
-import org.apache.lucene.search.Explanation;
-import org.apache.lucene.search.FilterWeight;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.MatchNoDocsQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.QueryVisitor;
 import org.apache.lucene.search.ScoreMode;
 import org.apache.lucene.search.Scorer;
 import org.apache.lucene.search.ScorerSupplier;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TwoPhaseIterator;
 import org.apache.lucene.search.Weight;
-import org.apache.lucene.util.BitDocIdSet;
-import org.apache.lucene.util.BitSet;
 import org.apache.lucene.util.BytesRef;
 import org.apache.lucene.util.DocIdSetBuilder;
-import org.apache.lucene.util.FixedBitSet;
 import org.apache.lucene.util.LongBitSet;
 
 import java.io.IOException;
@@ -39,16 +35,18 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.Set;
 
-public class TermNotInSetQuery extends Query {
+public class TermInSetQuery extends Query {
   private static final double J = 1.0;
   private static final double K = 1.0;
 
   private final String field;
   private final BytesRef[] terms;
+  private final int termsHashCode;
 
-  public TermNotInSetQuery(String field, Collection<BytesRef> terms) {
+  public TermInSetQuery(String field, Collection<BytesRef> terms) {
     this.field = field;
 
     final Set<BytesRef> uniqueTerms;
@@ -63,6 +61,7 @@ public class TermNotInSetQuery extends Query {
       assert it.hasNext();
       this.terms[i] = it.next();
     }
+    termsHashCode = Arrays.hashCode(this.terms);
   }
 
   @Override
@@ -83,8 +82,8 @@ public class TermNotInSetQuery extends Query {
       @Override
       public ScorerSupplier scorerSupplier(LeafReaderContext context) throws IOException {
 
-        if (terms.length == 0) {
-          return null;
+        if (terms.length <= 1) {
+          throw new IllegalStateException("Must call IndexSearcher#rewrite");
         }
 
         FieldInfo fi = context.reader().getFieldInfos().fieldInfo(field);
@@ -93,14 +92,13 @@ public class TermNotInSetQuery extends Query {
         }
 
         final long cost;
-        final long queryTermsCount = terms.length;
         Terms indexTerms = context.reader().terms(field);
         long potentialExtraCost = indexTerms.getSumDocFreq();
         final long indexedTermCount = indexTerms.size();
         if (indexedTermCount != -1) {
           potentialExtraCost -= indexedTermCount;
         }
-        cost = queryTermsCount + potentialExtraCost;
+        cost = terms.length + potentialExtraCost;
 
         Weight weight = this;
         return new ScorerSupplier() {
@@ -108,12 +106,12 @@ public class TermNotInSetQuery extends Query {
           @Override
           public Scorer get(long leadCost) throws IOException {
 
-            if (terms.length == 1) {
-              return docAtATimeScorer(weight, context);
-            }
+            assert terms.length > 1;
 
+            // If there are no doc values indexed, we have to use a postings-based approach:
             if (fi.getDocValuesType() != DocValuesType.SORTED_SET && fi.getDocValuesType() != DocValuesType.SORTED) {
               if (terms.length > IndexSearcher.getMaxClauseCount()) {
+                // TODO: There are times when we probably want to combine term/doc at a time:
                 return termAtATimeScorer(weight, context);
               } else {
                 return docAtATimeScorer(weight, context);
@@ -121,25 +119,40 @@ public class TermNotInSetQuery extends Query {
             }
 
             if (terms.length >= J * leadCost) {
+              // If there are a lot of terms relative to candidate docs, using DV is best:
               return docValuesScorer(weight, context);
             } else {
+              Terms t = context.reader().terms(field);
+              if (t == null) {
+                return null;
+              }
+
+              // Compute the "total density" of all term postings:
               long totalDensity = 0;
-              TermsEnum termsEnum = context.reader().terms(field).iterator();
+              TermsEnum termsEnum = t.iterator();
+              int fieldDocCount = t.getDocCount();
               TermState[] termStates = new TermState[terms.length];
               for (int i = 0; i < terms.length; i++) {
                 BytesRef term = terms[i];
                 if (termsEnum.seekExact(term) == false) {
                   termStates[i] = null;
                 } else {
+                  // If we find a "completely dense" postings list, we can use it directly:
+                  if (fieldDocCount == termsEnum.docFreq()) {
+                    PostingsEnum postings = termsEnum.postings(null, PostingsEnum.NONE);
+                    return docAtATimeScorer(weight, 1, new DisiWrapper(postings));
+                  }
                   totalDensity += termsEnum.docFreq();
                   termStates[i] = termsEnum.termState();
                 }
               }
 
-              if (totalDensity / (K * leadCost) >= 1.0) {
+              // Heuristic that determines whether-or-not to use DV or postings. Based on ratio
+              // of total density to candidate size:
+              if (totalDensity >= (K * leadCost)) {
                 return docValuesScorer(weight, context);
               } else {
-                return docAtATimeScorer(weight, context, termStates, termsEnum);
+                return docAtATimeScorer(weight, termsEnum, termStates);
               }
             }
           }
@@ -151,26 +164,29 @@ public class TermNotInSetQuery extends Query {
         };
       }
 
-      @Override
-      public Explanation explain(LeafReaderContext context, int doc) throws IOException {
-        return null;
-      }
-
       private Scorer docAtATimeScorer(Weight weight, LeafReaderContext context) throws IOException {
+        Terms t = context.reader().terms(field);
+        TermsEnum termsEnum = t.iterator();
+        int fieldDocCount = t.getDocCount();
+
         DisiWrapper[] disiWrappers = new DisiWrapper[terms.length];
         int disiCount = 0;
-        for (int i = 0; i < terms.length; i++) {
-          PostingsEnum postings = context.reader().postings(new Term(field, terms[i]), PostingsEnum.NONE);
-          if (postings != null) {
-            disiWrappers[i] = new DisiWrapper(postings);
+        for (BytesRef term : terms) {
+          if (termsEnum.seekExact(term)) {
+            PostingsEnum postings = termsEnum.postings(null, PostingsEnum.NONE);
+            DisiWrapper disiWrapper = new DisiWrapper(postings);
+            if (fieldDocCount == termsEnum.docFreq()) {
+              return docAtATimeScorer(weight, 1, disiWrapper);
+            }
+            disiWrappers[disiCount] = disiWrapper;
             disiCount++;
           }
         }
 
-        return docAtATimeScorer(weight, context, disiWrappers, disiCount);
+        return docAtATimeScorer(weight, disiCount, disiWrappers);
       }
 
-      private Scorer docAtATimeScorer(Weight weight, LeafReaderContext context, TermState[] termStates, TermsEnum termsEnum) throws IOException {
+      private Scorer docAtATimeScorer(Weight weight, TermsEnum termsEnum, TermState... termStates) throws IOException {
         DisiWrapper[] disiWrappers = new DisiWrapper[terms.length];
         int disiCount = 0;
         for (int i = 0; i < terms.length; i++) {
@@ -178,36 +194,49 @@ public class TermNotInSetQuery extends Query {
           if (termState != null) {
             termsEnum.seekExact(terms[i], termState);
             PostingsEnum postings = termsEnum.postings(null, PostingsEnum.NONE);
-            disiWrappers[i] = new DisiWrapper(postings);
+            disiWrappers[disiCount] = new DisiWrapper(postings);
             disiCount++;
           }
         }
 
-        return docAtATimeScorer(weight, context, disiWrappers, disiCount);
+        return docAtATimeScorer(weight, disiCount, disiWrappers);
       }
       
       private Scorer termAtATimeScorer(Weight weight, LeafReaderContext context) throws IOException {
-        DocIdSetBuilder hits = new DocIdSetBuilder(context.reader().maxDoc());
+        LeafReader reader = context.reader();
+
+        Terms t = reader.terms(field);
+        TermsEnum termsEnum = t.iterator();
+        int fieldDocCount = t.getDocCount();
+
+        DocIdSetIterator it = null;
+        DocIdSetBuilder hits = new DocIdSetBuilder(reader.maxDoc());
         boolean foundAtLeastOneTerm = false;
+        PostingsEnum postings = null;
         for (BytesRef term : terms) {
-          PostingsEnum postings = context.reader().postings(new Term(field, term), PostingsEnum.NONE);
-          if (postings != null) {
+          if (termsEnum.seekExact(term)) {
             foundAtLeastOneTerm = true;
+            postings = termsEnum.postings(postings, PostingsEnum.NONE);
+            if (fieldDocCount == termsEnum.docFreq()) {
+              it = postings;
+              break;
+            }
             hits.add(postings);
           }
         }
 
-        DocIdSetIterator it;
-        if (foundAtLeastOneTerm == false) {
-          it = DocIdSetIterator.empty();
-        } else {
-          it = hits.build().iterator();
+        if (it == null) {
+          if (foundAtLeastOneTerm == false) {
+            it = DocIdSetIterator.empty();
+          } else {
+            it = hits.build().iterator();
+          }
         }
 
         return new ConstantScoreScorer(weight, score(), scoreMode, it);
       }
 
-      private Scorer docAtATimeScorer(Weight weight, LeafReaderContext context, DisiWrapper[] disiWrappers, int disiCount) {
+      private Scorer docAtATimeScorer(Weight weight, int disiCount, DisiWrapper... disiWrappers) {
         final DocIdSetIterator it;
         if (disiCount == 0) {
           it = DocIdSetIterator.empty();
@@ -217,7 +246,11 @@ public class TermNotInSetQuery extends Query {
           DisiPriorityQueue pq = new DisiPriorityQueue(disiCount);
           pq.addAll(disiWrappers, 0, disiCount);
 
-          final long cost = Arrays.stream(disiWrappers).mapToLong(e -> e.cost).sum();
+          long c = 0;
+          for (int i = 0; i < disiCount; i++) {
+            c += disiWrappers[i].cost;
+          }
+          final long cost = c;
 
           it = new DocIdSetIterator() {
             private int docID = -1;
@@ -302,33 +335,64 @@ public class TermNotInSetQuery extends Query {
       
       @Override
       public boolean isCacheable(LeafReaderContext ctx) {
-        return DocValues.isCacheable(ctx, field);
+        return false;
       }
     };
   }
 
   @Override
   public Query rewrite(IndexSearcher indexSearcher) throws IOException {
-    return super.rewrite(indexSearcher);
+    if (terms.length == 0) {
+      return new MatchNoDocsQuery();
+    }
+    if (terms.length == 1) {
+      return new TermQuery(new Term(field, terms[0]));
+    }
+    return this;
   }
 
   @Override
   public void visit(QueryVisitor visitor) {
+    if (visitor.acceptField(field) == false) {
+      return;
+    }
 
+    final Term[] allTerms = new Term[terms.length];
+    for (int i = 0; i < terms.length; i++) {
+      allTerms[i] = new Term(field, terms[i]);
+    }
+    visitor.consumeTerms(this, allTerms);
   }
 
   @Override
   public String toString(String field) {
-    return null;
+    StringBuilder sb = new StringBuilder(field + ":(");
+    boolean first = true;
+    for (BytesRef t : terms) {
+      if (first == false) {
+        sb.append(' ');
+      }
+      sb.append(Term.toString(t));
+      first = false;
+    }
+    sb.append(')');
+    return sb.toString();
   }
 
   @Override
-  public boolean equals(Object obj) {
-    return false;
+  public boolean equals(Object other) {
+    if (other == this) {
+      return true;
+    }
+    if (sameClassAs(other) == false) {
+      return false;
+    }
+    TermInSetQuery o = (TermInSetQuery) other;
+    return field.equals(o.field) && termsHashCode == o.termsHashCode && Arrays.equals(terms, o.terms);
   }
 
   @Override
   public int hashCode() {
-    return 0;
+    return Objects.hash(field, termsHashCode);
   }
 }

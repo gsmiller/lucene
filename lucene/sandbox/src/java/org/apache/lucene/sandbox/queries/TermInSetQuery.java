06193e80e7fce88ec9901c1c8ae3c436a7eda812
Greg Miller <gsmiller@gmail.com>
2023-01-11 21:25:24 -0800

    add more ways to cheaply estimate cost and make unit test run for a long time but test lots of iters and docs

--- lucene/sandbox/src/java/org/apache/lucene/sandbox/queries/TermInSetQuery.java
+++ lucene/sandbox/src/java/org/apache/lucene/sandbox/queries/TermInSetQuery.java
@@ -87,110 +87,151 @@ public class TermInSetQuery extends Query {
           throw new IllegalStateException("Must call IndexSearcher#rewrite");
         }
 
-        LeafReader reader = context.reader();
-
         // If the field doesn't exist in the segment, return null:
+        LeafReader reader = context.reader();
         FieldInfo fi = reader.getFieldInfos().fieldInfo(field);
         if (fi == null) {
           return null;
         }
 
-        // Estimate the cost:
-        final long cost;
-        Terms indexTerms = reader.terms(field);
-        long potentialExtraCost = indexTerms.getSumDocFreq();
-        final long indexedTermCount = indexTerms.size();
-        if (indexedTermCount != -1) {
-          potentialExtraCost -= indexedTermCount;
-        }
-        cost = terms.length + potentialExtraCost;
-
-        Weight weight = this;
         return new ScorerSupplier() {
 
           @Override
           public Scorer get(long leadCost) throws IOException {
-
             assert terms.length > 1;
 
             // If there are no doc values indexed, we have to use a postings-based approach:
             if (fi.getDocValuesType() != DocValuesType.SORTED_SET
                 && fi.getDocValuesType() != DocValuesType.SORTED) {
-              return postingsScorer(weight, reader);
+              return postingsScorer(reader);
             }
 
-            if (terms.length >= J * leadCost) {
+            if (terms.length > J * leadCost) {
               // TODO: The actual term count could be lower if there are terms not in the segment.
-              // If there are a lot of terms relative to candidate docs, using DV is best:
-              return docValuesScorer(weight, context);
+              // If the number of terms is > the number of candidates, a DV should perform better:
+              return docValuesScorer(context);
             } else {
-              // TODO: Can we check this earlier with field infos?
               Terms t = reader.terms(field);
               if (t == null) {
-                return new ConstantScoreScorer(
-                    weight, score(), scoreMode, DocIdSetIterator.empty());
+                return docValuesScorer(context);
               }
+
+              // Assuming documents are randomly distributed (note: they may not be), as soon as
+              // a given term's postings list size is >= the number of candidate documents, we
+              // would expect a that term's postings to require advancing for every candidate doc
+              // (in the average case). This cost is multiplied out by the number of terms, so we
+              // can estimate the total amount of posting list advances we'll need to do by
+              // taking the sum of the postings sizes across all of our terms. Unfortunately, it's
+              // costly to get the actual postings sizes (we need to seek to each term), so we
+              // do a gross estimate up-front by computing the average postings size. Further
+              // complicating matters, we don't know how many of the terms are actually in the
+              // segment (we assume they all are), and of course, our candidate size is also an
+              // estimate:
+              double avgTermAdvances = Math.min(leadCost, (double) t.getSumDocFreq() / t.size());
+              double expectedTotalAdvances = avgTermAdvances * terms.length;
+              if (expectedTotalAdvances > leadCost) { // TODO: tuning coefficient?
+                return docValuesScorer(context);
+              }
+
+              // At this point, it seems that using a postings approach may be best, so we'll
+              // actually seek to all the terms and gather more accurate index statistics and make
+              // one more decision. The hope is that we end up using a postings approach since all
+              // this term seeking is wasted if we go DV.
 
               // Compute the "total density" of all term postings. This is wasted term-seeking work
               // if we end up using DVs. The good news is that the wasted work is proportional to
               // the number of terms, and if there are lots of terms, we hopefully don't drop into
               // this condition:
               long totalDensity = 0;
-              TermsEnum termsEnum = t.iterator();
+              int foundTermCount = 0;
               int fieldDocCount = t.getDocCount();
+              TermsEnum termsEnum = t.iterator();
               TermState[] termStates = new TermState[terms.length];
               TermState lastTermState = null;
               BytesRef lastTerm = null;
-              int foundTermCount = 0;
               for (int i = 0; i < terms.length; i++) {
                 BytesRef term = terms[i];
                 if (termsEnum.seekExact(term) == false) {
-                  termStates[i] = null;
                   continue;
                 }
 
                 // If we find a "completely dense" postings list, we can use it directly:
-                if (fieldDocCount == termsEnum.docFreq()) {
+                int termDocFreq = termsEnum.docFreq();
+                if (fieldDocCount == termDocFreq) {
                   PostingsEnum postings = termsEnum.postings(null, PostingsEnum.NONE);
-                  return new ConstantScoreScorer(weight, score(), scoreMode, postings);
+                  return scorerFor(postings);
                 }
 
-                totalDensity += termsEnum.docFreq();
-                lastTermState = termsEnum.termState();
-                termStates[i] = lastTermState;
-                lastTerm = term;
                 foundTermCount++;
+                totalDensity += Math.min(termDocFreq, leadCost);
+                lastTermState = termsEnum.termState();
+                lastTerm = term;
+                termStates[i] = lastTermState;
               }
 
               // We have some new information about how many terms were actually found in the
               // segment,
               // so make some special-case decisions based on that new information:
               if (foundTermCount == 0) {
-                return new ConstantScoreScorer(
-                    weight, score(), scoreMode, DocIdSetIterator.empty());
+                return emptyScorer();
               } else if (foundTermCount == 1) {
                 termsEnum.seekExact(lastTerm, lastTermState);
                 PostingsEnum postings = termsEnum.postings(null, PostingsEnum.NONE);
-                return new ConstantScoreScorer(weight, score(), scoreMode, postings);
+                return scorerFor(postings);
               }
 
               // TODO: Should we re-check a term count heuristic here now that we know how many
               // terms are actually in the segment?
               // Heuristic that determines whether-or-not to use DV or postings. Based on ratio
               // of total density to candidate size:
-              if (totalDensity >= (K * leadCost)) {
-                return docValuesScorer(weight, context);
+              if (totalDensity > (K * leadCost)) {
+                return docValuesScorer(context);
               } else {
-                return postingsScorer(weight, context.reader(), termsEnum, termStates);
+                return postingsScorer(context.reader(), termsEnum, termStates);
               }
             }
           }
 
           @Override
           public long cost() {
-            return cost;
+            // TODO: Should we lazily store this? Will #cost every be called multiple times?
+            try {
+              final long cost;
+              Terms indexTerms = reader.terms(field);
+              if (indexTerms == null) {
+                SortedSetDocValues dv = reader.getSortedSetDocValues(field);
+                if (dv == null) {
+                  return 0;
+                } else {
+                  return dv.cost();
+                }
+              }
+
+              long potentialExtraCost = indexTerms.getSumDocFreq();
+              final long indexedTermCount = indexTerms.size();
+              if (indexedTermCount != -1) {
+                potentialExtraCost -= indexedTermCount;
+              }
+              cost = terms.length + potentialExtraCost;
+
+              return cost;
+            } catch (IOException ioe) {
+              return Long.MAX_VALUE;
+            }
           }
         };
+      }
+
+      private Scorer emptyScorer() {
+        return new ConstantScoreScorer(this, score(), scoreMode, DocIdSetIterator.empty());
+      }
+
+      private Scorer scorerFor(DocIdSetIterator it) {
+        return new ConstantScoreScorer(this, score(), scoreMode, it);
+      }
+
+      private Scorer scorerFor(TwoPhaseIterator it) {
+        return new ConstantScoreScorer(this, score(), scoreMode, it);
       }
 
       private Scorer docAtATimeScorer(Weight weight, LeafReaderContext context) throws IOException {
@@ -232,6 +273,67 @@ public class TermInSetQuery extends Query {
         return docAtATimeScorer(weight, disiCount, disiWrappers);
       }
 
+      private Scorer docAtATimeScorer(Weight weight, int disiCount, DisiWrapper... disiWrappers) {
+        final DocIdSetIterator it;
+        if (disiCount == 0) {
+          it = DocIdSetIterator.empty();
+        } else if (disiCount == 1) {
+          it = disiWrappers[0].iterator;
+        } else {
+          DisiPriorityQueue pq = new DisiPriorityQueue(disiCount);
+          pq.addAll(disiWrappers, 0, disiCount);
+
+          long c = 0;
+          for (int i = 0; i < disiCount; i++) {
+            c += disiWrappers[i].cost;
+          }
+          final long cost = c;
+
+          it =
+              new DocIdSetIterator() {
+                private int docID = -1;
+
+                @Override
+                public int docID() {
+                  return docID;
+                }
+
+                @Override
+                public int nextDoc() throws IOException {
+                  return doNext(docID + 1);
+                }
+
+                @Override
+                public int advance(int target) throws IOException {
+                  return doNext(target);
+                }
+
+                private int doNext(int target) throws IOException {
+                  DisiWrapper top = pq.top();
+                  do {
+                    top.doc = top.approximation.advance(target);
+                    if (top.doc == target) {
+                      pq.updateTop();
+                      docID = target;
+                      return docID;
+                    }
+                    top = pq.updateTop();
+                  } while (top.doc < target);
+
+                  docID = top.doc;
+                  return docID;
+                }
+
+                @Override
+                public long cost() {
+                  return cost;
+                }
+              };
+        }
+
+        return new ConstantScoreScorer(weight, score(), scoreMode, it);
+      }
+
       private Scorer termAtATimeScorer(Weight weight, LeafReaderContext context)
           throws IOException {
         LeafReader reader = context.reader();
@@ -267,10 +369,10 @@ public class TermInSetQuery extends Query {
         return new ConstantScoreScorer(weight, score(), scoreMode, it);
       }
 
-      private Scorer postingsScorer(Weight weight, LeafReader reader) throws IOException {
+      private Scorer postingsScorer(LeafReader reader) throws IOException {
         Terms t = reader.terms(field);
         if (t == null) {
-          return new ConstantScoreScorer(weight, score(), scoreMode, DocIdSetIterator.empty());
+          return emptyScorer();
         }
 
         TermsEnum termsEnum = t.iterator();
@@ -279,13 +381,12 @@ public class TermInSetQuery extends Query {
         for (int i = 0; i < terms.length; i++) {
           BytesRef term = terms[i];
           if (termsEnum.seekExact(term) == false) {
-            termStates[i] = null;
             continue;
           }
 
           if (fieldDocCount == termsEnum.docFreq()) {
             PostingsEnum postings = termsEnum.postings(null, PostingsEnum.NONE);
-            return new ConstantScoreScorer(weight, score(), scoreMode, postings);
+            return scorerFor(postings);
           }
 
           termStates[i] = termsEnum.termState();
@@ -293,11 +394,10 @@ public class TermInSetQuery extends Query {
 
         // TODO: how much cost are we adding by not loading the postings inline and instead saving
         // term states?
-        return postingsScorer(weight, reader, termsEnum, termStates);
+        return postingsScorer(reader, termsEnum, termStates);
       }
 
-      private Scorer postingsScorer(
-          Weight weight, LeafReader reader, TermsEnum termsEnum, TermState... termStates)
+      private Scorer postingsScorer(LeafReader reader, TermsEnum termsEnum, TermState... termStates)
           throws IOException {
         int foundTermCount = 0;
         int disiWrapperCount = 0;
@@ -387,71 +487,10 @@ public class TermInSetQuery extends Query {
               };
         }
 
-        return new ConstantScoreScorer(weight, score(), scoreMode, it);
+        return scorerFor(it);
       }
 
-      private Scorer docAtATimeScorer(Weight weight, int disiCount, DisiWrapper... disiWrappers) {
-        final DocIdSetIterator it;
-        if (disiCount == 0) {
-          it = DocIdSetIterator.empty();
-        } else if (disiCount == 1) {
-          it = disiWrappers[0].iterator;
-        } else {
-          DisiPriorityQueue pq = new DisiPriorityQueue(disiCount);
-          pq.addAll(disiWrappers, 0, disiCount);
-
-          long c = 0;
-          for (int i = 0; i < disiCount; i++) {
-            c += disiWrappers[i].cost;
-          }
-          final long cost = c;
-
-          it =
-              new DocIdSetIterator() {
-                private int docID = -1;
-
-                @Override
-                public int docID() {
-                  return docID;
-                }
-
-                @Override
-                public int nextDoc() throws IOException {
-                  return doNext(docID + 1);
-                }
-
-                @Override
-                public int advance(int target) throws IOException {
-                  return doNext(target);
-                }
-
-                private int doNext(int target) throws IOException {
-                  DisiWrapper top = pq.top();
-                  do {
-                    top.doc = top.approximation.advance(target);
-                    if (top.doc == target) {
-                      pq.updateTop();
-                      docID = target;
-                      return docID;
-                    }
-                    top = pq.updateTop();
-                  } while (top.doc < target);
-
-                  docID = top.doc;
-                  return docID;
-                }
-
-                @Override
-                public long cost() {
-                  return cost;
-                }
-              };
-        }
-
-        return new ConstantScoreScorer(weight, score(), scoreMode, it);
-      }
-
-      private Scorer docValuesScorer(Weight weight, LeafReaderContext context) throws IOException {
+      private Scorer docValuesScorer(LeafReaderContext context) throws IOException {
         SortedSetDocValues dv = DocValues.getSortedSet(context.reader(), field);
 
         boolean hasAtLeastOneTerm = false;
@@ -465,7 +504,7 @@ public class TermInSetQuery extends Query {
         }
 
         if (hasAtLeastOneTerm == false) {
-          return new ConstantScoreScorer(weight, score(), scoreMode, DocIdSetIterator.empty());
+          return emptyScorer();
         }
 
         TwoPhaseIterator it =
@@ -486,7 +525,7 @@ public class TermInSetQuery extends Query {
               }
             };
 
-        return new ConstantScoreScorer(weight, score(), scoreMode, it);
+        return scorerFor(it);
       }
 
       @Override

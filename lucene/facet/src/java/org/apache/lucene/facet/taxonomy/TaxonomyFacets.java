db1c324a013f64cf83aa5c8e81ddf3a47608d957
Greg Miller <gsmiller@gmail.com>
2023-06-30 12:22:17 -0700

    Initialize facet counting data structures lazily.
    This change covers:
    * Taxonomy faceting
      * FastTaxonomyFacetCounts
      * TaxonomyFacetIntAssociations
      * TaxonomyFacetFloatAssociations
    * SSDV faceting
      * SortedSetDocValuesFacetCounts
      * ConcurrentSortedSetDocValuesFacetCounts
      * StringValueFacetCounts
    * Range faceting:
      * LongRangeFacetCounts
      * DoubleRangeFacetCounts
    * Long faceting:
      * LongValueFacetCounts
    
    Left for a future iteration:
    * RangeOnRange faceting
    * FacetSet faceting

--- lucene/facet/src/java/org/apache/lucene/facet/taxonomy/TaxonomyFacets.java
+++ lucene/facet/src/java/org/apache/lucene/facet/taxonomy/TaxonomyFacets.java
@@ -19,11 +19,13 @@ package org.apache.lucene.facet.taxonomy;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import org.apache.lucene.facet.FacetResult;
 import org.apache.lucene.facet.Facets;
+import org.apache.lucene.facet.FacetsCollector;
 import org.apache.lucene.facet.FacetsConfig;
 import org.apache.lucene.facet.FacetsConfig.DimConfig;
 
@@ -62,6 +64,9 @@ public abstract class TaxonomyFacets extends Facets {
   /** {@code FacetsConfig} provided to the constructor. */
   protected final FacetsConfig config;
 
+  /** {@code FacetsCollector} provided to the constructor. */
+  final FacetsCollector fc;
+
   /** Maps parent ordinal to its child, or -1 if the parent is childless. */
   private int[] children;
 
@@ -72,11 +77,12 @@ public abstract class TaxonomyFacets extends Facets {
   protected final int[] parents;
 
   /** Sole constructor. */
-  protected TaxonomyFacets(String indexFieldName, TaxonomyReader taxoReader, FacetsConfig config)
+  protected TaxonomyFacets(String indexFieldName, TaxonomyReader taxoReader, FacetsConfig config, FacetsCollector fc)
       throws IOException {
     this.indexFieldName = indexFieldName;
     this.taxoReader = taxoReader;
     this.config = config;
+    this.fc = fc;
     parents = taxoReader.getParallelTaxonomyArrays().parents();
   }
 
@@ -147,6 +153,11 @@ public abstract class TaxonomyFacets extends Facets {
   @Override
   public List<FacetResult> getAllDims(int topN) throws IOException {
     validateTopN(topN);
+
+    if (hasValues() == false) {
+      return Collections.emptyList();
+    }
+
     int[] children = getChildren();
     int[] siblings = getSiblings();
     int ord = children[TaxonomyReader.ROOT_ORDINAL];
@@ -167,4 +178,7 @@ public abstract class TaxonomyFacets extends Facets {
     results.sort(BY_VALUE_THEN_DIM);
     return results;
   }
+
+  /** Were any values actually aggregated during counting? */
+  abstract boolean hasValues();
 }

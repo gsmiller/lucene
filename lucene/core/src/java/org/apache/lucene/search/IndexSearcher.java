35d6b2f7caea56ae114cea36e391f580eb9d6939
Greg Miller <gmiller@amazon.com>
2024-08-14 18:31:30 -0700

    is regressing: move stuff into calling method

--- lucene/core/src/java/org/apache/lucene/search/IndexSearcher.java
+++ lucene/core/src/java/org/apache/lucene/search/IndexSearcher.java
@@ -630,17 +630,17 @@ public class IndexSearcher {
    */
   public <C extends Collector, T> T search(Query query, CollectorManager<C, T> collectorManager)
       throws IOException {
+    final LeafSlice[] leafSlices = getSlices();
+    final List<C> collectors = new ArrayList<>(leafSlices.length);
     final C firstCollector = collectorManager.newCollector();
+    collectors.add(firstCollector);
     query = rewrite(query, firstCollector.scoreMode().needsScores());
     final Weight weight = createWeight(query, firstCollector.scoreMode(), 1);
-    return search(weight, collectorManager, firstCollector);
+    return search(weight, collectorManager, firstCollector, collectors, leafSlices);
   }
 
   private <C extends Collector, T> T search(
-      Weight weight, CollectorManager<C, T> collectorManager, C firstCollector) throws IOException {
-    final LeafSlice[] leafSlices = getSlices();
-    final List<C> collectors = new ArrayList<>(leafSlices.length);
-    collectors.add(firstCollector);
+      Weight weight, CollectorManager<C, T> collectorManager, C firstCollector, List<C> collectors, LeafSlice[] leafSlices) throws IOException {
     if (leafSlices.length == 0) {
       // there are no segments, nothing to offload to the executor, but we do need to call reduce to
       // create some kind of empty result
